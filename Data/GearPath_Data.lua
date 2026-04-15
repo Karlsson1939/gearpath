35b932abd515d230da62679a95905c2bef22e5da
Tony Karlsson-Kristersson <karlsson-1993@hotmail.com>
2026-04-15 21:42:38 +0200

    M2: Add Elemental Shaman BiS data

--- Data/GearPath_Data.lua
+++ Data/GearPath_Data.lua
@@ -1,1 +1,233 @@
--- GearPath: GearPath_Data.lua (stub)
+-- GearPath
+-- Data/GearPath_Data.lua - BiS tables (Midnight Season 1)
+-- Source: Noxxic/Icy Veins, updated March 2026
+
+GearPath.BiSData = {}
+
+GearPath.BiSData["SHAMAN"] = {}
+GearPath.BiSData["SHAMAN"]["Elemental"] = {
+
+    -- [1] Head
+    [1] = {
+        itemID     = 249979,
+        itemName   = "Locus of the Primal Core",
+        slotID     = 1,
+        source     = "Voidspire",
+        sourceType = "RAID",
+        sourceName = "The Voidspire",
+        bossName   = "Vaelgor the Fallen",
+        ilvl       = 252,
+        priority   = 9,
+        isTier     = true,
+    },
+
+    -- [2] Neck
+    [2] = {
+        itemID     = 250247,
+        itemName   = "Amulet of the Abyssal Hymn",
+        slotID     = 2,
+        source     = "MarchOnQueldanas",
+        sourceType = "RAID",
+        sourceName = "March on Quel'Danas",
+        bossName   = "Midnight Falls",
+        ilvl       = 252,
+        priority   = 8,
+        isTier     = false,
+    },
+
+    -- [3] Shoulders
+    [3] = {
+        itemID     = 249980,
+        itemName   = "Tempests of the Primal Core",
+        slotID     = 3,
+        source     = "Voidspire",
+        sourceType = "RAID",
+        sourceName = "The Voidspire",
+        bossName   = "Council of Silvermoon",
+        ilvl       = 252,
+        priority   = 9,
+        isTier     = true,
+    },
+
+    -- [5] Chest
+    [5] = {
+        itemID     = 249982,
+        itemName   = "Embrace of the Primal Core",
+        slotID     = 5,
+        source     = "Voidspire",
+        sourceType = "RAID",
+        sourceName = "The Voidspire",
+        bossName   = "Xal'atath",
+        ilvl       = 252,
+        priority   = 9,
+        isTier     = true,
+    },
+
+    -- [6] Waist
+    [6] = {
+        itemID     = 244611,
+        itemName   = "World Tender's Barkclasp",
+        slotID     = 6,
+        source     = "Crafted",
+        sourceType = "CRAFTED",
+        sourceName = "Crafted",
+        bossName   = nil,
+        ilvl       = 265,
+        priority   = 7,
+        isTier     = false,
+    },
+
+    -- [7] Legs
+    [7] = {
+        itemID     = 251215,
+        itemName   = "Greaves of the Divine Guile",
+        slotID     = 7,
+        source     = "MarchOnQueldanas",
+        sourceType = "RAID",
+        sourceName = "March on Quel'Danas",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 7,
+        isTier     = false,
+    },
+
+    -- [8] Feet
+    [8] = {
+        itemID     = 244610,
+        itemName   = "World Tender's Rootslippers",
+        slotID     = 8,
+        source     = "Crafted",
+        sourceType = "CRAFTED",
+        sourceName = "Crafted",
+        bossName   = nil,
+        ilvl       = 265,
+        priority   = 7,
+        isTier     = false,
+    },
+
+    -- [9] Wrists
+    [9] = {
+        itemID     = 249304,
+        itemName   = "Fallen King's Cuffs",
+        slotID     = 9,
+        source     = "MarchOnQueldanas",
+        sourceType = "RAID",
+        sourceName = "March on Quel'Danas",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 6,
+        isTier     = false,
+    },
+
+    -- [10] Hands
+    [10] = {
+        itemID     = 249983,
+        itemName   = "Earthgrips of the Primal Core",
+        slotID     = 10,
+        source     = "Voidspire",
+        sourceType = "RAID",
+        sourceName = "The Voidspire",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 9,
+        isTier     = true,
+    },
+
+    -- [11] Finger 1
+    [11] = {
+        itemID     = 193708,
+        itemName   = "Platinum Star Band",
+        slotID     = 11,
+        source     = "AlgetharAcademy",
+        sourceType = "DUNGEON",
+        sourceName = "Algeth'ar Academy",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 7,
+        isTier     = false,
+    },
+
+    -- [12] Finger 2
+    [12] = {
+        itemID     = 249919,
+        itemName   = "Sin'dorei Band of Hope",
+        slotID     = 12,
+        source     = "MarchOnQueldanas",
+        sourceType = "RAID",
+        sourceName = "March on Quel'Danas",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 7,
+        isTier     = false,
+    },
+
+    -- [13] Trinket 1
+    [13] = {
+        itemID     = 249343,
+        itemName   = "Gaze of the Alnseer",
+        slotID     = 13,
+        source     = "Dreamrift",
+        sourceType = "RAID",
+        sourceName = "Dreamrift",
+        bossName   = "Chimaerus",
+        ilvl       = 252,
+        priority   = 10,
+        isTier     = false,
+    },
+
+    -- [14] Trinket 2
+    [14] = {
+        itemID     = 250144,
+        itemName   = "Emberwing Feather",
+        slotID     = 14,
+        source     = "WindrunnerSpire",
+        sourceType = "DUNGEON",
+        sourceName = "Windrunner Spire",
+        bossName   = "Emberdawn",
+        ilvl       = 252,
+        priority   = 10,
+        isTier     = false,
+    },
+
+    -- [15] Back
+    [15] = {
+        itemID     = 249974,
+        itemName   = "Guardian of the Primal Core",
+        slotID     = 15,
+        source     = "Voidspire",
+        sourceType = "RAID",
+        sourceName = "The Voidspire",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 6,
+        isTier     = false,
+    },
+
+    -- [16] Main Hand
+    [16] = {
+        itemID     = 251083,
+        itemName   = "Excavating Cudgel",
+        slotID     = 16,
+        source     = "Crafted",
+        sourceType = "CRAFTED",
+        sourceName = "Crafted",
+        bossName   = nil,
+        ilvl       = 265,
+        priority   = 10,
+        isTier     = false,
+    },
+
+    -- [17] Off Hand
+    [17] = {
+        itemID     = 251105,
+        itemName   = "Ward of the Spellbreaker",
+        slotID     = 17,
+        source     = "MarchOnQueldanas",
+        sourceType = "RAID",
+        sourceName = "March on Quel'Danas",
+        bossName   = nil,
+        ilvl       = 252,
+        priority   = 9,
+        isTier     = false,
+    },
+}
