54091878d70988e4aec6a3709abf0e5a1793a1fb
Tony Karlsson-Kristersson <karlsson-1993@hotmail.com>
2026-04-15 21:33:34 +0200

    M2: Add slot weight multipliers

--- Data/SlotWeights.lua
+++ Data/SlotWeights.lua
@@ -1,1 +1,30 @@
--- GearPath: SlotWeights.lua (stub)
+-- GearPath
+-- Data/SlotWeights.lua - Default slot weight multipliers
+
+GearPath.DefaultSlotWeights = {
+    -- Weapons
+    [16] = 2.0, -- Main Hand
+    [17] = 2.0, -- Off Hand
+
+    -- Trinkets
+    [13] = 1.8,
+    [14] = 1.8,
+
+    -- Tier set slots
+    [1]  = 1.5, -- Head
+    [3]  = 1.5, -- Shoulders
+    [5]  = 1.5, -- Chest
+    [10] = 1.5, -- Hands
+    [7]  = 1.5, -- Legs
+
+    -- Jewelry
+    [2]  = 1.2, -- Neck
+    [11] = 1.2, -- Finger 1
+    [12] = 1.2, -- Finger 2
+
+    -- Other armor
+    [15] = 1.0, -- Back
+    [9]  = 1.0, -- Wrists
+    [6]  = 1.0, -- Waist
+    [8]  = 1.0, -- Feet
+}
